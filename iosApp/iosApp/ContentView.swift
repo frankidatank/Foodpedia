63984e29cac08ca0b155b8b3ddea297b25219804
Frankie Murillo <frankidatank@gmail.com>
2021-11-30 15:27:23 -0800

    Working on UI

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -9,14 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Recipe+")
-            .font(.largeTitle)
-            .fontWeight(.ultraLight)
-            .multilineTextAlignment(.center)
-            .padding()
+        VStack {
+            Spacer()
+            welcomeTitile()
+            Spacer()
+            enterBar()
+            Spacer()
+        }
     }
     
 }
+
+struct enterBar: View {
+    @State var textFieldInput : String = ""
+    @State var dataArr : [String] = []
+    var body: some View {
+        Text("Enter your ingredients")
+        
+        VStack {
+            HStack {
+            TextField("Type something here...", text: $textFieldInput)
+            .padding()
+            .background(Color.white.cornerRadius(15))
+            .font(.headline)
+            
+            Button(action: {
+                saveInput()
+            }, label: {
+                Text("+")
+                .padding()
+                .background(.green)
+                .cornerRadius(10)
+            })
+            
+            }
+            
+            Text("in your fridge, you have...")
+                .font(.footnote)
+                .fontWeight(.thin)
+                .multilineTextAlignment(.leading)
+        ForEach(dataArr, id: \.self){ data in
+            Text(data)
+            }
+            
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Text("Find Recipes ➡️")
+                .padding()
+                .background(.green)
+                .cornerRadius(10)
+            })
+    
+        }
+    }
+    
+    func saveInput() {
+        dataArr.append(textFieldInput)
+        textFieldInput = ""
+    }
+        
+}
+
+struct welcomeTitile: View {
+    var body: some View {
+        Text("Welcome to Recipe+")
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .padding(.trailing, 0.0)
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
