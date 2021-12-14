6ccd9ebfc112b16b36e03618e6f652f4aa91b0e2
Dev Soni <dsoni@mail.sfsu/edu>
2021-12-14 15:18:12 -0800

    Changing file structure

---
+++ Foodpedia/Controller/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Foodpedia
+//
+//  Created by FoodPedia on 14/11/21.
+//
+
+//
+//  ContentView.swift
+//  foodped
+//
+//  Created by Dev Soni on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
