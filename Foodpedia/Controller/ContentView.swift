a7cd26cf35e9cc8b4bd8b481eb66b5926d341b37
Dev Soni <dsoni@mail.sfsu/edu>
2021-12-14 16:58:00 -0800

    Creating main screen

--- Foodpedia/Controller/ContentView.swift
+++ Foodpedia/Controller/ContentView.swift
@@ -2,27 +2,50 @@
 //  ContentView.swift
 //  Foodpedia
 //
-//  Created by FoodPedia on 14/11/21.
-//
-
-//
-//  ContentView.swift
-//  foodped
-//
-//  Created by Dev Soni on 12/14/21.
+//  Created by FoodPedia on 12/14/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var navigator : Navigator
+    @ObservedObject var isSearching = IsSearching.shared
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                
+                if navigator.isSearchPageOpen {
+                    
+                    
+                }else if navigator.isReciepeListOpen {
+                    
+                    
+                }else{
+              
+                }
+                
+                
+                Spacer()
+                
+               
+            }
+            
+            if isSearching.isSearching{
+            ZStack{
+                Color(UIColor.init(white: 0.3, alpha: 0.7))
+                    .ignoresSafeArea()
+                ProgressView()
+                }
+            }
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(Navigator())
     }
 }
